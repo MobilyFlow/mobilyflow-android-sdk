c3670bcb8a0700810c9ae0eb895d7d47e5b7a5bc
LaGregance <gregoire.taja@gmail.com>
2025-02-26 15:28:57 +0100

    Fix build.gradle

--- mobilyflow-android-test-sdk/build.gradle.kts
+++ mobilyflow-android-test-sdk/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.android.build.gradle.internal.scope.publishBuildArtifacts
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("maven-publish")
 }
 
 android {
@@ -60,4 +63,18 @@ dependencies {
     implementation(libs.androidx.runtime.livedata)
 
     implementation(project(":mobilyflow-android-sdk"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "com.github.MobilyFlow"
+            artifactId = "mobilyflow-android-sdk"
+            version = "0.0.1"
+
+            afterEvaluate {
+                from(components.findByName("release"))
+            }
+        }
+    }
 }
