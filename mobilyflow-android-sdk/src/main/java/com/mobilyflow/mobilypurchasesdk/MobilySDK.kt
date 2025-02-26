de1cfda784a8c29c32be4da05abbca206b0ee0b2
LaGregance <gregoire.taja@gmail.com>
2025-02-26 12:20:53 +0100

    Initial commit

---
+++ mobilyflow-android-sdk/src/main/java/com/mobilyflow/mobilypurchasesdk/MobilySDK.kt
@@ -0,0 +1,9 @@
+package com.mobilyflow.mobilypurchasesdk
+
+import android.util.Log
+
+class MobilySDK {
+    fun hello() {
+        Log.d("MobilyFlow", "Hello World")
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Mobilyflow Android SDK"
+include(":mobilyflow-android-sdk")
+include(":mobilyflow-android-test-sdk")
