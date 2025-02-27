1e74b1cb9f082fcb25a32c8d1eca52073a3d2d8c
LaGregance <gregoire.taja@gmail.com>
2025-02-27 11:27:31 +0100

    Publish on Maven Central

--- mobilyflow-android-sdk/build.gradle.kts
+++ mobilyflow-android-sdk/build.gradle.kts
@@ -1,7 +1,10 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    id("maven-publish")
+
+    id("com.vanniktech.maven.publish") version ("0.30.0")
 }
 
 android {
@@ -45,16 +48,43 @@ dependencies {
     implementation(libs.billing)
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("release") {
-            groupId = "com.github.MobilyFlow"
-            artifactId = "mobilyflow-android-sdk"
-            version = "0.0.5"
+//project.setProperty("signingInMemoryKey", System.getenv("ORG_GRADLE_PROJECT_signingInMemoryKey"))
+//project.setProperty("signingInMemoryKeyId", System.getenv("ORG_GRADLE_PROJECT_signingInMemoryKeyId"))
+//project.setProperty("signingInMemoryKeyPassword", System.getenv("ORG_GRADLE_PROJECT_signingInMemoryKeyPassword"))
 
-            afterEvaluate {
-                from(components.findByName("release"))
+//println("signingInMemoryKey: " + project.findOptionalProperty("signingInMemoryKey"))
+//println("signingInMemoryKeyId: " + project.findProperty("signingInMemoryKeyId"))
+//println("signingInMemoryKeyPassword: " + project.findProperty("signingInMemoryKeyPassword"))
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
+    signAllPublications()
+
+    coordinates("com.mobilyflow.mobilypurchasesdk", "mobilyflow-android-sdk", "0.0.1")
+
+    pom {
+        name.set("MobilyFlow Android SDK")
+        description.set("MobilyFlow Android SDK")
+        inceptionYear.set("2025")
+        url.set("https://github.com/MobilyFlow/mobilyflow-android-sdk")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
             }
+        }
+        developers {
+            developer {
+                id.set("MobilyFlow")
+                name.set("MobilyFlow")
+                url.set("https://github.com/MobilyFlow/")
+            }
+        }
+        scm {
+            url.set("https://github.com/MobilyFlow/mobilyflow-android-sdk")
+            connection.set("scm:git:git://github.com/MobilyFlow/mobilyflow-android-sdk.git")
+            developerConnection.set("scm:git:ssh://git@github.com/MobilyFlow/mobilyflow-android-sdk.git")
         }
     }
 }
