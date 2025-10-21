5f902f929fb3852cfa013f3e44b8e5f170f1803e
LaGregance <gregoire.taja@gmail.com>
2025-10-21 09:52:59 +0200

    Version 0.3.0-alpha.2

--- mobilyflow-android-sdk/build.gradle.kts
+++ mobilyflow-android-sdk/build.gradle.kts
@@ -44,7 +44,7 @@ mavenPublishing {
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
     signAllPublications()
 
-    coordinates("com.mobilyflow", "mobilyflow-android-sdk", "0.3.0-alpha.1")
+    coordinates("com.mobilyflow", "mobilyflow-android-sdk", "0.3.0-alpha.2")
 
     pom {
         name.set("MobilyFlow Android SDK")
