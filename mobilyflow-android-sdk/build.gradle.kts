aed07e73dbcd3f0683c58278492fe3cb00c61e1f
LaGregance <gregoire.taja@gmail.com>
2025-11-12 12:40:54 +0100

    Version 0.4.1-alpha.8

--- mobilyflow-android-sdk/build.gradle.kts
+++ mobilyflow-android-sdk/build.gradle.kts
@@ -46,7 +46,7 @@ mavenPublishing {
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
     signAllPublications()
 
-    coordinates("com.mobilyflow", "mobilyflow-android-sdk", "0.4.1-alpha.7")
+    coordinates("com.mobilyflow", "mobilyflow-android-sdk", "0.4.1-alpha.8")
 
     pom {
         name.set("MobilyFlow Android SDK")
